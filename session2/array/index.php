9dc8d8186928e90ae13997a5c3d3c112e18b5430
xuanbach1992 <bachax1992@gmail.com>
2019-10-06 22:56:57 +0700

    demo array

--- session2/array/index.php
+++ session2/array/index.php
@@ -1,1 +1,11 @@
-<?php
+<script>
+    <?php
+    $arrNumber = [];
+    for ($i = 0; $i < 10; $i++) {
+        $inputNum = rand(1, 10);
+        array_push($arrNumber, $inputNum);
+    }
+//    print_r($arrNumber);
+    ?>
+    document.write(<?php print_r($arrNumber)?>);
+</script>
