0fd64e2d71704b9b05d766911e330dfa1298b22d
xuanbach1992 <bachax1992@gmail.com>
2019-10-06 10:16:14 +0700

    stop watch

--- session3/stopWatch/stopWatch.php
+++ session3/stopWatch/stopWatch.php
@@ -1,34 +1,45 @@
 <?php
-
-
 class StopWatch
 {
-    private $__timeStart;
-    private $__timeEnd;
+    private $startTime;
+    private $endTime;
 
-    public function __construct($__timeStart, $__timeEnd)
+    function __construct()
     {
-        $this->start = $__timeStart;
-        $this->end = $__timeEnd;
+        $this->startTime;
+        $this->endTime;
     }
 
-    public function getStart()
+    public function getStartTime()
     {
-        return $this->start;
+        return $this->startTime;
     }
 
-    public function getEnd()
+    public function getEndTime()
     {
-        return $this->end;
+        return $this->endTime;
     }
 
     public function start()
     {
-        return date("Y-M-d h:m:s");
+        $this->startTime = "2019-10-03 10:44:00";
     }
 
     public function end()
     {
-        return date("Y-M-d h:m:s");
+        $this->endTime = date("y-m-d H:i:s");
+    }
+
+    public function getElapsedTime()
+    {
+        $this->startTime = date_parse_from_format('Y-m-d H:i:s', $this->startTime);
+        $time_stamp1 = mktime($this->endTime['hour'], $this->endTime['minute'], $this->endTime['second'], $this->endTime['month']
+            , $this->endTime['day'], $this->endTime['year']);
+        echo $time_stamp1 . "<br>";
+        echo $this->endTime;
+        return ($this->end() - $time_stamp1);
     }
 }
+
+$objStopWatch = new StopWatch();
+echo $objStopWatch->getElapsedTime();
