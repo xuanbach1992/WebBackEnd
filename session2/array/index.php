27fcfb631c9ee41cead241eef3b6ce53fc02e0db
xuanbach1992 <bachax1992@gmail.com>
2019-10-06 23:06:02 +0700

    demo array merge

--- session2/array/index.php
+++ session2/array/index.php
@@ -1,11 +1,28 @@
 <script>
-    <?php
-    $arrNumber = [];
-    for ($i = 0; $i < 10; $i++) {
-        $inputNum = rand(1, 10);
-        array_push($arrNumber, $inputNum);
-    }
-//    print_r($arrNumber);
-    ?>
-    document.write(<?php print_r($arrNumber)?>);
+    <!--    --><?php
+    //    $arrNumber = [];
+    //    for ($i = 0; $i < 10; $i++) {
+    //        $inputNum = rand(1, 10);
+    //        array_push($arrNumber, $inputNum);
+    //    }
+    ////    print_r($arrNumber);
+    //    ?>
+    //    document.write(<?php //print_r($arrNumber)?>//);
 </script>
+<?php
+$arraySum = [];
+$arrayFirst = [];
+$arraySecond = [];
+for ($i = 0; $i < 5; $i++) {
+    array_push($arrayFirst, rand(1, 10));
+}
+for ($j = 0; $j < 5; $j++) {
+    array_push($arraySecond, rand(1, 10));
+}
+print_r($arrayFirst);
+echo "<br>";
+print_r($arraySecond);
+echo "<br>";
+$arraySum=array_merge($arrayFirst,$arraySecond);
+print_r($arraySum);
+?>
