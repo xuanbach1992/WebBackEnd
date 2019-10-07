ac49c437bfb2bf1062b6b71389531a88b05f2fab
xuanbach1992 <Xuanbach1992>
2019-10-07 14:41:57 +0700

    demo interface and abstract

---
+++ session4/abstract_demo/AbtractAndInterfaceTest.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'Animals/Chicken.php';
+include_once 'Animals/Tiger.php';
+include_once 'Fruits/Apple.php';
+include_once 'Fruits/Orange.php';
+
+echo ('---- Animals<br>');
+
+$animals[0] = new Tiger();
+$animals[1] = new Chicken();
+
+foreach ($animals as $animal) {
+    echo $animal->makeSound() . '<br>';
+
+    if ($animal instanceof Chicken) {
+        echo $animal->howToEat() . '<br>';
+    }
+}
+
+echo ('---- Fruits<br>');
+
+$fruits[0] = new Apple();
+$fruits[1] = new Orange();
+
+foreach ($fruits as $fruit) {
+    echo $fruit->howToEat() . '<br>';
+}
