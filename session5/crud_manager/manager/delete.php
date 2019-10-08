bcf12b7e87af146036209e9b71762c0f54f5d782
xuanbach1992 <bachax1992@gmail.com>
2019-10-08 21:11:13 +0700

    fix css

--- session5/crud_manager/manager/delete.php
+++ session5/crud_manager/manager/delete.php
@@ -4,7 +4,7 @@ $id = $_GET['id'];
 
 $data = file_get_contents('data.json');
 $data = json_decode($data, true);
-    array_splice($data,$id,1);
+array_splice($data, $id, 1);
 $data = json_encode($data);
 file_put_contents('data.json', $data);
 header("Location: list.php");
